23f801af82abb4c0093f2d0e69a8ad95781de22e
Jeshan <jeshankhn6@gmail.com>
2019-05-09 00:19:24 +0545

    second commit

--- routes/routes.js
+++ routes/routes.js
@@ -5,7 +5,15 @@ var router = express.Router();
 
 router.get("/", (req, res) => {
   Todo.find({}).then((result)=>{
-    res.render("index",{ todos: result})
+    let todo = result.filter((result)=>{
+      return !result.done;
+    })
+
+    let doneTodo = result.filter((result)=>{
+      return result.done;
+    })
+
+    res.render("index",{ todos: todo, donetodos: doneTodo})
   })
 })
 
@@ -23,6 +31,19 @@ router.post("/todo", (req, res) => {
     console.log(err);
     res.redirect("/");
   })
+})
+
+router.post('/todo/:id/completed', (req, res)=>{
+  let todoId = req.params.id;
+
+  Todo.findById(todoId)
+    .exec()
+    .then((result)=>{
+      result.done = !result.done;
+      return result.save()
+    }).then((result)=>{
+      res.redirect('/');
+    })
 })
 
 module.exports = router;
